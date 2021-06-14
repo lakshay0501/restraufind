4aed581d2b89b0a9d04b8f928842f633ed4a28b7
Lakshay Arora <lakshay.arora05012001@gmail.com>
2021-06-15 00:47:21 +0530

    ready to deploy

--- yel.js
+++ yel.js
@@ -121,6 +121,9 @@ app.use((err,req,res,next) => {
     if(!err.message) err.message = 'Oh no, Something went wrong!!'
     res.status(statuscode).render('error',{err})
 })
-app.listen(3000 , () => {
+
+const port = process.env.PORT || 3000;
+
+app.listen(port , () => {
     console.log('Serving on port 3000!!')
 })
